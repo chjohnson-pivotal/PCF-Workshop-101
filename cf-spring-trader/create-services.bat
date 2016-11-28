b0e8182740807d4686da8aa0f3d6784d9a24a32a
Rick Ross <rross@pivotal.io>
2016-11-28 10:32:09 -0500

    Added create-services.bat

---
+++ cf-spring-trader/create-services.bat
@@ -0,0 +1,4 @@
+cf create-service p-mysql 100mb-dev trader-db
+cf create-service p-service-registry standard trader-registry
+cf create-service p-config-server standard config-server
+cf create-service p-circuit-breaker-dashboard standard trader-dashboard
