0a71fa22d6dbedf9ad1ffb62d05abdc2bec98245
Rick Ross <rross@pivotal.io>
2016-11-28 11:09:41 -0500

    Updated create-services

--- cf-spring-trader/create-services.bat
+++ cf-spring-trader/create-services.bat
@@ -1,4 +1,4 @@
 cf create-service p-mysql 100mb-dev trader-db
 cf create-service p-service-registry standard trader-registry
-cf create-service p-config-server standard config-server
+cf create-service p-config-server standard config-server -c "{\"git\": { \"uri\": \"https://github.com/rossr3-pivotal/trader-config-repo.git\" } }"
 cf create-service p-circuit-breaker-dashboard standard trader-dashboard
--- cf-spring-trader/create-services.sh
+++ cf-spring-trader/create-services.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cf create-service p-mysql 100mb-dev trader-db
 cf create-service p-service-registry standard trader-registry
-cf create-service p-config-server standard config-server
+cf create-service p-config-server standard config-server -c '{"git": { "uri": "https://github.com/rossr3-pivotal/trader-config-repo.git" } }'
 cf create-service p-circuit-breaker-dashboard standard trader-dashboard
